83df033cadcdfd445f5f9c705a7ab576d35a298f
Ewa Utracka <ewczi@Ewas-MacBook-Pro.local>
2019-09-14 17:21:34 +0200

    mockup modal added

--- script.js
+++ script.js
@@ -1,99 +1,104 @@
 "use strict";
 
-// GLOBAL VARIABLES
-const myLink = "http://petlatkea.dk/2019/students1991.json";
-const parent = document.querySelector("ol");
-const template = document.querySelector("template");
-const modal = document.querySelector(".modal");
-const img = document.querySelector(".header-img");
-const bg = document.querySelector(".background");
+//INITIAL SETUP
+let students = [];
+let filter = "all";
+let sort;
 
+//OBJECT CONTAINING ALL GLOBAL DOM VARIABLES
+const DOM = {
+  jsonLink: "http://petlatkea.dk/2019/students1991.json",
+  parent: document.querySelector("ol"),
+  template: document.querySelector("template"),
+  wrapperDiv: document.querySelector(".background"),
+  filterDropdown: document.querySelectorAll("#filter"),
+  sortDropdown: document.querySelectorAll("#sort")
+};
+
+//CALLING INIT FUNCTION ON PAGE LOAD
 document.addEventListener("DOMContentLoaded", init);
 
 function init() {
+  setUpEventListeners();
+  backgroundFade();
   loadJSON();
-  setTimeout(backgroundFade, 200);
+}
 
-  //SETUP
-  let students = [];
-  let filter = "all";
-  let sort;
-
-  //EVENT FOR SORT AND FILTER SELECTION
-  document.querySelectorAll("#filter").forEach(option => {
+//SETTING UP EVENT LISTENERS FOR FILTER AND SORT
+function setUpEventListeners() {
+  DOM.filterDropdown.forEach(option => {
     option.addEventListener("change", filterBy);
   });
-  document.querySelectorAll("#sort").forEach(option => {
+  DOM.sortDropdown.forEach(option => {
     option.addEventListener("change", sortBy);
   });
+}
 
-  //FETCHING DATA FROM JSON
-  async function loadJSON() {
-    let jsonData = await fetch(myLink);
-    students = await jsonData.json();
-    //DISPLAY LIST
-    displayData();
-  }
+//FETCHING DATA FROM JSON
+async function loadJSON() {
+  let jsonData = await fetch(DOM.jsonLink);
+  students = await jsonData.json();
+  //DISPLAY LIST
+  displayList();
+}
 
-  //DISPLAYING THE LIST
-  function displayData() {
-    console.log(students);
-    parent.innerHTML = "";
+//FILTER DATA FUNCTION
+function filterBy() {
+  filter = this.value;
+  displayList();
+}
 
-    students.forEach(object => {
-      //SEPARATING FIRST NAME AND LAST NAME
-      const firstSpace = object.fullname.indexOf(" ");
-      let firstName = object.fullname.substring(0, firstSpace);
-      let lastName = object.fullname.substring(firstSpace + 1);
-      object.firstName = firstName;
-      object.lastName = lastName;
+//SORT DATA FUNCTION
+function sortBy() {
+  sort = this.value;
 
-      //IF STATEMENT FOR FILTERING THE LIST
-      if (filter == object.house || filter == "all") {
-        //cloning the template
-        const clone = template.cloneNode(true).content;
-
-        //populating it
-        clone.querySelector("li").innerHTML =
-          firstName + " " + lastName + ", " + object.house;
-
-        //appending to DOM
-        parent.appendChild(clone); // puts the tamplate in my <ol>
-      }
+  //IF STATEMENTS + .SORT() METHOD
+  if (sort == "firstName") {
+    students.sort(function(a, b) {
+      return a.firstName.localeCompare(b.firstName);
     });
+  } else if (sort == "lastName") {
+    students.sort(function(a, b) {
+      return a.lastName.localeCompare(b.lastName);
+    });
+  } else if (sort == "house") {
+    students.sort(function(a, b) {
+      return a.house.localeCompare(b.house);
+    });
+  } else if (sort == "all") {
+    init(); //RESETING
   }
+  displayList();
+}
 
-  //FILTER DATA FUNCTION
-  function filterBy() {
-    filter = this.value;
-    displayData();
-  }
+//DISPLAYING THE LIST
+function displayList() {
+  DOM.parent.innerHTML = "";
 
-  //SORT DATA FUNCTION
-  function sortBy() {
-    sort = this.value;
+  students.forEach(object => {
+    //SEPARATING FIRST NAME AND LAST NAME
+    const firstSpace = object.fullname.indexOf(" ");
+    let firstName = object.fullname.substring(0, firstSpace);
+    let lastName = object.fullname.substring(firstSpace + 1);
+    object.firstName = firstName;
+    object.lastName = lastName;
 
-    //IF STATEMENTS + .SORT() METHOD
-    if (sort == "firstName") {
-      students.sort(function(a, b) {
-        return a.firstName.localeCompare(b.firstName);
-      });
-    } else if (sort == "lastName") {
-      students.sort(function(a, b) {
-        return a.lastName.localeCompare(b.lastName);
-      });
-    } else if (sort == "house") {
-      students.sort(function(a, b) {
-        return a.house.localeCompare(b.house);
-      });
-    } else if (sort == "all") {
-      init(); //RESETING
+    //IF STATEMENT FOR FILTERING THE LIST
+    if (filter == object.house || filter == "all") {
+      //cloning the template
+      const clone = DOM.template.cloneNode(true).content;
+
+      //populating it
+      clone.querySelector("li").innerHTML =
+        firstName + " " + lastName + ", " + object.house;
+
+      //appending to DOM
+      DOM.parent.appendChild(clone); // puts the tamplate in my <ol>
     }
-    displayData();
-  }
+  });
 }
 
 //FADING BACKGROUND IMAGE WITH DELAY
 function backgroundFade() {
-  bg.style.opacity = "0.5";
+  DOM.wrapperDiv.style.opacity = "0.2";
 }
